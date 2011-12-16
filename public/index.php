dac49f809ff933fcf07a2d8c92de3d3c1490004b
Ivan Mosiev <ivan.mosiev@altexsoft.com>
2011-12-16 04:04:54 +0200

    added some simple code to index

--- public/index.php
+++ public/index.php
@@ -1,2 +1,16 @@
 <?php
 require_once 'bootstrap.php';
+
+$config = include 'config.php';
+
+$router = new Router();
+
+$controller = $router->getController();
+
+$action = $router->getAction();
+
+$args = $router->getArgs();
+
+$contr = new $controller;
+
+call_user_func_array(array($contr, $action), $args);
